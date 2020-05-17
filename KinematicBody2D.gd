4c5d6f47cf9c21ec49c5f54e075d42263432e8c5
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2020-05-17 21:41:39 +0300

    Set animation after velocity

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -5,29 +5,31 @@ const SPEED = 140
 const GRAVITY = 10
 const JUMPFORCE = -400
 
-var motion = Vector2(0, 0)
+var velocity = Vector2(0, 0)
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
-		$Sprite.play("walk")
+		velocity.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		velocity.x = -SPEED
+		
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMPFORCE
+	else:
+		velocity.y += GRAVITY
+	
+	velocity = move_and_slide(velocity, UP)
+
+	velocity.x = lerp(velocity.x, 0, 0.1)
+
+	if not is_on_floor():
+		$Sprite.play("air")
+	elif velocity.x > 0 and int(velocity.x) > 20:
+		$Sprite.play("walk")
+	elif velocity.x < 0 and int(velocity.x) < -20:
 		$Sprite.play("walk")
 	else:
 		$Sprite.play("idle")
 		
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		motion.y = JUMPFORCE
-	else:
-		motion.y += GRAVITY
+	$Sprite.flip_h = velocity.x < 0
 	
-	
-	motion = move_and_slide(motion, UP)
-	
-	if (motion.y != 0):
-		$Sprite.play("air")
-		
-	$Sprite.flip_h = motion.x < 0
-		
-	motion.x = lerp(motion.x, 0, 0.1)
