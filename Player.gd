2dde8244dff989131994257a6df4fe680d7677ec
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2020-05-18 18:37:51 +0300

    Rename player script file. Add restart to world script.

--- Player.gd
+++ Player.gd
@@ -1,11 +1,11 @@
 extends KinematicBody2D
 
-const UP = Vector2(0, -1)
+const UP = Vector2.UP
 const SPEED = 140
-const GRAVITY = 10
+const GRAVITY = 500
 const JUMPFORCE = -400
 
-var velocity = Vector2(0, 0)
+var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
@@ -16,7 +16,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMPFORCE
 	else:
-		velocity.y += GRAVITY
+		velocity.y += GRAVITY * delta
 	
 	velocity = move_and_slide(velocity, UP)
 
